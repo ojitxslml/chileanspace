c427e31fa56b398254c824b82bd9e3ba7cc2871d
Juan Pablo Urra <jp.urra42@gmail.com>
2025-10-04 23:47:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1133592454-4ca42",
+  "appId": "1:1091692577367:web:92fca1f7da9f408ac89912",
+  "apiKey": "AIzaSyBPkWjZsJZ9_eBHY2TGKxRi05j0BjoNnCE",
+  "authDomain": "studio-1133592454-4ca42.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1091692577367"
+};
